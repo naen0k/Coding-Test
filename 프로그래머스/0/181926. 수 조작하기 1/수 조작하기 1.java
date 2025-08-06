f4a07fe590b93bd1236328c8e706b537b527a572
장하은 <jnaen0k@gmail.com>
2025-08-06 11:45:49 +0900

    [level 0] Title: 수 조작하기 1, Time: 74.69 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, String control) {
+        String[] str = control.split("");
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].equals("w"))
+                n++;
+            else if(str[i].equals("s"))
+                n--;
+            else if(str[i].equals("d"))
+                n+=10;
+            else if(str[i].equals("a"))
+                n-=10;
+        }
+        return n;
+    }
+}
