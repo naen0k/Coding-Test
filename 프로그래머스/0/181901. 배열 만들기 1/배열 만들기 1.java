b916075678c0cfe476e490d2df1e3f05e6dc8dfe
장하은 <jnaen0k@gmail.com>
2025-08-07 17:31:51 +0900

    [level 0] Title: 배열 만들기 1, Time: 11.41 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % k == 0)
+                cnt++;
+        }
+        int[] answer = new int[cnt];
+        int idx = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % k == 0) {
+                answer[idx] = i;
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
