1c1cda8e3269fd40981494bde4f80e3bbb7759c2
장하은 <jnaen0k@gmail.com>
2026-05-02 22:17:08 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = [];
+    for(let i of my_string) {
+        if(!isNaN(i) && i !== ' ')
+            answer.push(Number(i));
+    }
+    return answer.sort((a, b) => a - b);
+}
